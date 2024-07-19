20c810e2649319b925045f998d466dcfcf8fee77
RiZainou <m.toun976@gmail.com>
2024-07-19 09:49:14 +0200

    feat: add About page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,7 +1,41 @@
 export default function AboutPage() {
   return (
     <div>
-      <h1>je suis about page</h1>
+      <div className="border-2 border-blue-500 rounded-lg shadow-lg mx-80 mt-5 p-5">
+        <h2 className="text-center text-2xl mt-4 font-black text-gray-900">
+          Style de jeu
+        </h2>
+        <p className="pt-4 text-center text-gray-700">
+          Les Snow Kids sont connus pour leur jeu rapide et leur coordination
+          impressionnante. Leur style se caractérise par des passes précises,
+          des mouvements fluides et une stratégie défensive solide. Ils
+          utilisent souvent des techniques spéciales liées à leur planète
+          glaciale, comme des passes rapides qui simulent des mouvements de
+          glace.
+        </p>
+
+        <h2 className="text-center text-2xl mt-4 font-black text-gray-900">
+          Objectifs et défis
+        </h2>
+        <p className="pt-4 text-center text-gray-700">
+          L’équipe des Snow Kids a pour objectif de remporter le championnat de
+          Glaktik Football tout en surmontant divers défis et obstacles. Ils
+          doivent souvent faire face à des équipes adverses redoutables et
+          naviguer à travers des situations complexes qui mettent à l'épreuve
+          leur cohésion et leur résilience.
+        </p>
+
+        <h2 className="text-center text-2xl mt-4 font-black text-gray-900">
+          Valeurs et esprit d'équipe
+        </h2>
+        <p className="pt-4 text-center text-gray-700">
+          Les Snow Kids se distinguent par leur esprit de camaraderie et leur
+          détermination collective. Ils croient en l'importance du travail
+          d'équipe et de la persévérance pour atteindre leurs objectifs. Leur
+          voyage est aussi une quête pour prouver leur valeur et défendre leur
+          planète d'origine.
+        </p>
+      </div>
     </div>
   );
 }
